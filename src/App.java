ba28152db5b30038c4e691dc0673e3102acb32e1
dianabello <leidyaparicio2703@gmail.com>
2025-08-26 21:29:24 -0500

    Actualiza App.java y App.class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-       
+    public static void main(String[] args) {
+
+        Libro miLibro = new Libro("El Principito", "Antoine de Saint-Exupéry", 92);
+        miLibro.mostrarFicha();
     }
 }
